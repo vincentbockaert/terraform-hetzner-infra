20ce113d6a5b635039ca6e952f8eaf85c006d49b
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-05-22 22:43:59 +0200

    typo output

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "cloudflare_ip_ranges" {
 
 # hetzner cloud
 
-output "hetzner_fedora_reachability" {
-    value = "Servername: ${hcloud_server.primary_selfhost.name}\nIPV4: ${hcloud_server.primary_selfhost.ipv4_address}\nIPV6: ${hcloud_server.primary_selfhost.ipv6_address}"
+output "hetzner_primary_selfhost_reachability" {
+  value = "Servername: ${hcloud_server.primary_selfhost.name}\nIPV4: ${hcloud_server.primary_selfhost.ipv4_address}\nIPV6: ${hcloud_server.primary_selfhost.ipv6_address}"
 }
 
